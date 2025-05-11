9743fd370cf421e02dd61c5c44a74e9a3bb19675
mangorange517 <mango20080517@gmail.com>
2025-05-12 08:54:31 +0900

    Add files via upload

---
+++ p105.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int n;
+int f(){
+    int t=123;
+    return t;
+}
+int main(){
+    int s=0;
+    scanf("%d", &n);
+    for(int i=1; i<=n;i++){
+        s=s+f();
+}
+    printf("%d\n",s);
+}
